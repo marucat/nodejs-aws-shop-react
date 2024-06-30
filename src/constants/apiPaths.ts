3784349556d15d74a4253ab11879a903f0010d91
marucat <belolutskaya@gmail.com>
2024-07-01 01:59:49 +0300

    small fix

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://7msla2inm6.execute-api.us-east-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://529eg0vs50.execute-api.us-east-1.amazonaws.com/prod/",
+  import: "https://529eg0vs50.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
